006f8587bbc3c8b1f02f6f7d16f44cb140f199d2
Marcin Czop <marcin@mczop.eu>
2024-11-18 12:51:01 +0100

    Add Grafana Loki logging to production

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,33 @@
 import pino from "pino";
-import PinoPretty from "pino-pretty";
 
-const logger = pino({
-	transport: {
+let transport;
+
+if (process.env.NODE_ENV === "production") {
+	transport = pino.transport({
+		target: "pino-loki",
+		options: {
+			batching: true,
+			interval: 5,
+			host: process.env.LOKI_HOST,
+			basicAuth: {
+				username: process.env.LOKI_USER,
+				password: process.env.LOKI_PASS,
+			},
+			labels: {
+				app: "YALS",
+			},
+		},
+	});
+} else {
+	transport = pino.transport({
 		target: "pino-pretty",
 		options: {
 			colorize: true,
 			minimumLevel: "debug",
 		},
-	},
-});
+	});
+}
+
+const logger = pino(transport);
 
 export default logger;
